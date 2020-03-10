0db2e1ac819de27323fee86a5bce9a001b136572
Alexander Filippov <61475285+af69@users.noreply.github.com>
2020-03-10 18:09:58 +0200

    Delete LaunchingFromCMD.java

--- books_ex/editions/se/certification/[1] oca_java_se_8_programmer/chapter_1/LaunchingFromCMD.java
+++
@@ -1,8 +0,0 @@
-public class LaunchingFromCMD {
-    public static void main(String[] args) {
-        System.out.println(args[0] + " " + args[1]);
-    }
-
-    // javac MyClass x 1
-    // output: x 1
-}
