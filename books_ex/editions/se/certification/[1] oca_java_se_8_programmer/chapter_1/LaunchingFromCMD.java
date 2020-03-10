e872e4b15413d51744dff6ae98ec28af6696b654
Alexander Filippov <61475285+af69@users.noreply.github.com>
2020-03-10 18:08:55 +0200

    Add files via upload

---
+++ books_ex/editions/se/certification/[1] oca_java_se_8_programmer/chapter_1/LaunchingFromCMD.java
@@ -0,0 +1,8 @@
+public class LaunchingFromCMD {
+    public static void main(String[] args) {
+        System.out.println(args[0] + " " + args[1]);
+    }
+
+    // javac MyClass x 1
+    // output: x 1
+}
